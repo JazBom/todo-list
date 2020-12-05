206632c53b4b9d54a9b3e539a8143fd423384a89
Jessica Bommer <xininu@gmail.com>
2020-12-05 16:53:45 +1100

    commit submit button code

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("App starts");
+const todoList = [];
+const onTodoFormSubmit = (event) => {
+    event.preventDefault();
+    const $todo = $('input[name="todo"]');
+    const todoItem = {title: $todo.val(), done: false}
+    todoList.push(todoItem);
+    console.log('todoList', todoList);
+};
+$("#todo-form").on("submit", onTodoFormSubmit);
